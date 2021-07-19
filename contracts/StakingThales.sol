101dd64005e2b2e09965bb76a862af9f2957d667
kantevski <kantevsk@pa.uc3m.es>
2021-07-19 18:03:23 +0200

    Modified StakingThales.sol

--- contracts/StakingThales.sol
+++ contracts/StakingThales.sol
@@ -20,25 +20,23 @@ contract StakingThales is IERC20, Owned, ReentrancyGuard, Pausable {
     IERC20 public stakingToken;
     IERC20 public feeToken;
 
-    uint public weeksOfStakingFromDayZero = 0;
+    uint public weeksOfStaking = 0;
     uint public lastPeriod = 0;
     uint public durationPeriod = 7 days;
     uint public startTime = 0;
+    uint public rewardsForLastWeek = 0;
+    uint public rewardFeesForLastWeek = 0;
 
-    mapping(address => uint) public rewards;
-    mapping(address => uint) public rewardFees;
     mapping(address => uint) public stakerRewardsClaimed;
     mapping(address => uint) public stakerFeesClaimed;
 
     uint private _totalSupply;
+    uint private _totalRewardsClaimed;
     mapping(address => uint) private _balances;
-    mapping(address => uint) private _stakersPointers;
-    mapping(address => uint) private _stakingTime;
-    mapping(address => uint) private _stakingWeek;
-    mapping(address => uint) private _discountOnLateStaking;
-    mapping(address => bool) private _stakerCannotClaimRewardsOrStake;
+    mapping(address => uint) private _lastStakingWeek;
+    mapping(address => uint) private _lastRewardsClaimed;
+    mapping(address => bool) private _stakerCannotStake;
 
-    address[] private _stakers;
 
     /* ========== CONSTRUCTOR ========== */
 
@@ -62,21 +60,64 @@ contract StakingThales is IERC20, Owned, ReentrancyGuard, Pausable {
     function balanceOf(address account) external view returns (uint) {
         return _balances[account];
     }
+    
+    function getUnclaimedRewards(address account) external view returns (uint) {
+        return calculateUnclaimedRewards(account);
+
+    }
+    function getUnclaimedRewardFees(address account) external view returns (uint) {
+        return calculateUnclaimedFees(account);
+
+    }
+
+    function getAlreadyClaimedRewards(address account) external view returns (uint) {
+        return stakerRewardsClaimed[account];
+    }
+
+    function getAlreadyClaimedFees(address account) external view returns (uint) {
+        return stakerFeesClaimed[account];
+    }
+
+    /* ========== PUBLIC ========== */
+
+    function startStakingPeriod() external onlyOwner {
+        require(startTime == 0, "Staking has already started");
+        startTime = block.timestamp;
+        weeksOfStaking = 0;
+        lastPeriod = startTime;
+        durationPeriod = 7 days;
+    }
+
+    function closePeriod() external nonReentrant notPaused {
+        require(startTime > 0, "Staking period has not started");
+        require(
+            block.timestamp >= lastPeriod.add(durationPeriod), 
+            "7 days has not passed since the last closed period");
+
+        lastPeriod = block.timestamp;
+        weeksOfStaking = weeksOfStaking.add(1);
+        //Actions taken on every closed period
+        rewardsForLastWeek = calculateRewardsForWeek(weeksOfStaking);
+        rewardFeesForLastWeek = calculateFeesForWeek(weeksOfStaking);
+
+        emit ClosedPeriod(weeksOfStaking, lastPeriod);
+    }
 
     function stake(uint amount) external nonReentrant notPaused {
+        require(startTime > 0, "Staking period has not started");
         require(amount > 0, "Cannot stake 0");
-        if (isStaker(msg.sender)) {
-            require(
-                _stakerCannotClaimRewardsOrStake[msg.sender] == false,
-                "Cannot stake, the staker is paused from staking due to withdrawal of rewards"
-            );
-            // claimRewards();
-        } else {
-            registerStaker(msg.sender);
+        require( 
+            _stakerCannotStake[msg.sender] == false,
+            "Cannot stake, the staker is paused from staking due to withdrawal of rewards"
+        );
+        // Check if there are not claimable rewards from last week. 
+        // Claim them, and add new stake
+        if(_lastRewardsClaimed[msg.sender] < weeksOfStaking) {
+            claimReward();
         }
         _totalSupply = _totalSupply.add(amount);
         _balances[msg.sender] = _balances[msg.sender].add(amount);
-        _stakingTime[msg.sender] = block.timestamp;
+        _lastStakingWeek[msg.sender] = weeksOfStaking;
         stakingToken.safeTransferFrom(msg.sender, address(this), amount);
         emit Staked(msg.sender, amount);
     }
@@ -84,57 +125,68 @@ contract StakingThales is IERC20, Owned, ReentrancyGuard, Pausable {
     function unstake() external {}
 
     function claimReward() public nonReentrant notPaused {
-        require(isStaker(msg.sender), "The account is not staker");
-        require((lastPeriod >= _stakingTime[msg.sender].add(28 days)), "4 weeks has not passed since the last stake");
-        uint unclaimedReward = CalculateUnclaimedRewards(msg.sender);
-        rewards[msg.sender] = rewards[msg.sender].add(unclaimedReward);
-        emit RewardsClaimed(msg.sender, unclaimedReward);
-    }
-
-    function CalculateUnclaimedRewards(address account) public view returns (uint) {
-        require(isStaker(msg.sender), "The account is not a staker");
-        //TO BE added
-    }
-
-    function startStakingPeriod() external onlyOwner {
-        require(startTime == 0, "Staking has already started");
-        startTime = block.timestamp;
-        weeksOfStakingFromDayZero = 0;
-        lastPeriod = startTime;
-        durationPeriod = 7 days;
-    }
-
-    function closePeriod() external nonReentrant notPaused {
         require(startTime > 0, "Staking period has not started");
-        require(block.timestamp >= lastPeriod.add(durationPeriod), "7 days has not passed since the last closed period");
+        
+        //Calculate rewards
+        uint unclaimedReward = calculateUnclaimedRewards(msg.sender);
+        uint unclaimedFees = calculateUnclaimedFees(msg.sender);
 
-        //Actions taken on every closed period
-
-        lastPeriod = block.timestamp;
-        weeksOfStakingFromDayZero = weeksOfStakingFromDayZero.add(1);
-        emit ClosedStakingPeriod(weeksOfStakingFromDayZero, lastPeriod);
+        if(unclaimedReward > 0) {
+            _totalRewardsClaimed = _totalRewardsClaimed.add(unclaimedReward);
+            // Stake the newly claimed reward:
+            _totalSupply = _totalSupply.add(unclaimedReward);
+            _balances[msg.sender] = _balances[msg.sender].add(unclaimedReward);
+            _lastStakingWeek[msg.sender] = weeksOfStaking;
+            // Transfer to Escrow contract
+            stakingToken.addToEscrow(msg.sender, unclaimedReward);
+            // Record the total claimed rewards
+            stakerRewardsClaimed[msg.sender] = stakerRewardsClaimed[msg.sender].add(unclaimedReward);
+            emit RewardsClaimed(msg.sender, unclaimedReward);
+        } if(unclaimedFees > 0) {
+            feeToken.transferFrom(address(this), msg.sender, unclaimedFees);
+            stakerFeesClaimed[msg.sender] = stakerFeesClaimed[msg.sender].add(unclaimedFees);
+            emit FeeRewardsClaimed(msg.sender, unclaimedFees);
+        }
+        // Update last claiming week
+        _lastRewardsClaimed[msg.sender] = weeksOfStaking;
     }
+
+
 
     /* ========== INTERNAL FUNCTIONS ========== */
 
-    function isStaker(address account) internal view returns (bool) {
+    function calculateUnclaimedRewards(address account) internal view returns (uint){
         require(account != address(0), "Invalid account address used");
-        if (_stakers.length == 0) {
-            return false;
-        }
-        return (_stakers[_stakersPointers[account]] == account);
+        require(_balances[account] > 0, "Account is not a staker");
+        require(_lastRewardsClaimed[account] < weeksOfStaking, "Rewards already claimed for last week");
+
+        // return _balances[account].div(1e18).div(_totalSupply).mul(rewardsForLastWeek);
+        return _balances[account].div(_totalSupply).mul(rewardsForLastWeek);
     }
 
-    function registerStaker(address account) internal nonReentrant notPaused {
+    function calculateUnclaimedFees(address account) internal view returns (uint) {
         require(account != address(0), "Invalid account address used");
-        require(_stakers[_stakersPointers[account]] != account, "Staker already registered");
-        _stakersPointers[msg.sender] = _stakers.push(msg.sender).sub(1);
+        require(_balances[account] > 0, "Account is not a staker");
+        require(_lastRewardsClaimed[account] < weeksOfStaking, "Rewards already claimed for last week");
+
+        // return _balances[account].div(1e18).div(_totalSupply).mul(rewardFeesForLastWeek);
+        return _balances[account].div(_totalSupply).mul(rewardFeesForLastWeek);
+    }
+
+    function calculateRewardsForWeek(uint week) internal view returns (uint) {
+        //ADD formula
+        return 0;
+    }
+    function calculateFeesForWeek(uint week) internal view returns (uint) {
+        //ADD formula
+        return 0;
     }
 
     /* ========== EVENTS ========== */
 
     event RewardAdded(uint reward);
     event Staked(address indexed user, uint amount);
-    event ClosedStakingPeriod(uint WeekOfStaking, uint lastPeriod);
+    event ClosedPeriod(uint WeekOfStaking, uint lastPeriod);
     event RewardsClaimed(address account, uint unclaimedReward);
+    event FeeRewardsClaimed(address account, uint unclaimedFees);
 }

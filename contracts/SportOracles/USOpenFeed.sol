ab430893055ff8d1bdefa4a190439a585f38e96c
dgornjakovic <dgornjakovic87@gmail.com>
2021-08-31 14:23:40 +0200

    US open markets

---
+++ contracts/SportOracles/USOpenFeed.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.5.16;
+
+import "@chainlink/contracts/src/v0.5/ChainlinkClient.sol";
+import "synthetix-2.43.1/contracts/Owned.sol";
+
+contract USOpenFeed is ChainlinkClient, Owned {
+    using Chainlink for Chainlink.Request;
+
+    address public oracle;
+    bytes32 public jobId;
+    uint256 public fee;
+
+    uint public result;
+
+    string public season;
+
+    constructor(
+        address _owner,
+        address _oracle,
+        bytes32 _jobId,
+        uint256 _fee,
+        string memory _season
+    ) public Owned(_owner) {
+        //remove for the test
+        setPublicChainlinkToken();
+        oracle = _oracle;
+        jobId = _jobId;
+        fee = _fee;
+        season = _season;
+    }
+
+    function setOracle(address _oracle) external onlyOwner {
+        oracle = _oracle;
+    }
+
+    function setJobId(bytes32 _jobId) external onlyOwner {
+        jobId = _jobId;
+    }
+
+    function setFee(uint256 _fee) external onlyOwner {
+        fee = _fee;
+    }
+
+    function setSeason(string calldata _season) external onlyOwner {
+        season = _season;
+    }
+
+    function setResult(uint _result) external onlyOwner {
+        _setResult(_result);
+    }
+
+    /**
+     * Initial request
+     */
+    function requestSportsWinner() external {
+        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillSportsWinner.selector);
+        req.add("season", season);
+        sendChainlinkRequestTo(oracle, req, fee);
+    }
+
+    /**
+     * Callback function
+     */
+    function fulfillSportsWinner(bytes32 _requestId, uint _result) external recordChainlinkFulfillment(_requestId) {
+        _setResult(_result);
+    }
+
+    function _setResult(uint _result) internal {
+        result = _result;
+    }
+
+    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+}

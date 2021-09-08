10c0441b332ec7a559fb6840c7b6714df5b633e3
Ajla Musovic <ajla.msvc@gmail.com>
2021-09-08 15:02:48 +0200

    issuers script update

--- scripts/snx-data/issuers.js
+++ scripts/snx-data/issuers.js
@@ -5,7 +5,7 @@ const ethers = require('ethers');
 
 const { issued } = require('./util');
 
-const FIRST_BLOCK = 5000000; // Jan-30-2018
+const FIRST_BLOCK = 5855000; // Jun-26-2018
 const BLOCKS_PER_WEEK = 45000; // approx
 let txCount = 0;
 
@@ -15,7 +15,7 @@ async function getBlocks() {
 	const currentBlockNumber = (await provider.getBlock()).number;
 	console.log('currentBlockNumber', currentBlockNumber);
 
-	for(let i = FIRST_BLOCK; i < currentBlockNumber; i = i+BLOCKS_PER_WEEK) {
+	for(let i = FIRST_BLOCK; i < currentBlockNumber + BLOCKS_PER_WEEK; i = i+BLOCKS_PER_WEEK) {
 		blocks.push(i);
 	}
 	

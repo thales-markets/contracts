7a748d286cf8d5f59e3a719a43aad996e72a0c0b
Ajla Musovic <ajla.msvc@gmail.com>
2021-11-25 14:35:28 +0100

    update PriceFeed; add inheritance

--- contracts/PriceFeed/PriceFeed.sol
+++ contracts/PriceFeed/PriceFeed.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.16;
 
 // Inheritance
 import "../interfaces/IPriceFeed.sol";
-import "@openzeppelin/upgrades-core/contracts/Initializable.sol";
+import "./OwnedInitializable.sol";
 // Libraries
 import "synthetix-2.50.4-ovm/contracts/SafeDecimalMath.sol";
 
@@ -11,7 +11,7 @@ import "synthetix-2.50.4-ovm/contracts/SafeDecimalMath.sol";
 // AggregatorInterface from Chainlink represents a decentralized pricing network for a single currency key
 import "@chainlink/contracts-0.0.10/src/v0.5/interfaces/AggregatorV2V3Interface.sol";
 
-contract PriceFeed is IPriceFeed, Initializable {
+contract PriceFeed is IPriceFeed, OwnedInitializable {
     using SafeMath for uint;
     using SafeDecimalMath for uint;
 
@@ -25,9 +25,7 @@ contract PriceFeed is IPriceFeed, Initializable {
     address public nominatedOwner;
 
     function initialize(address _owner) public initializer {
-        require(_owner != address(0), "Owner address cannot be 0");
-        owner = _owner;
-        emit OwnerChanged(address(0), _owner);
+        OwnedInitializable.initialize((_owner));
     }
 
     /* ========== MUTATIVE FUNCTIONS ========== */
@@ -117,30 +115,7 @@ contract PriceFeed is IPriceFeed, Initializable {
         }
     }
 
-    function nominateNewOwner(address _owner) external onlyOwner {
-        nominatedOwner = _owner;
-        emit OwnerNominated(_owner);
-    }
-
-    function acceptOwnership() external {
-        require(msg.sender == nominatedOwner, "You must be nominated before you can accept ownership");
-        emit OwnerChanged(owner, nominatedOwner);
-        owner = nominatedOwner;
-        nominatedOwner = address(0);
-    }
-
-    modifier onlyOwner() {
-        _onlyOwner();
-        _;
-    }
-
-    function _onlyOwner() private view {
-        require(msg.sender == owner, "Only the contract owner may perform this action");
-    }
-
     /* ========== EVENTS ========== */
     event AggregatorAdded(bytes32 currencyKey, address aggregator);
     event AggregatorRemoved(bytes32 currencyKey, address aggregator);
-    event OwnerNominated(address newOwner);
-    event OwnerChanged(address oldOwner, address newOwner);
 }

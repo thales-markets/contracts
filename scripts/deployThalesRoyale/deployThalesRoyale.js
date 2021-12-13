730782c6897f76b954909019ee4b7e4893cf2e1d
Gruja <gruja.work@gmail.com>
2021-12-13 17:59:00 +0100

    adding version royale proxy init

--- scripts/deployThalesRoyale/deployThalesRoyale.js
+++ scripts/deployThalesRoyale/deployThalesRoyale.js
@@ -4,13 +4,19 @@ const w3utils = require('web3-utils');
 
 const { toBytes32 } = require('../../index');
 
-const { getTargetAddress, setTargetAddress } = require('../helpers');
+const { getTargetAddress, setTargetAddress, encodeCall } = require('../helpers');
 
 async function main() {
-	let accounts = await ethers.getSigners();
-	let owner = accounts[0];
+
+    /* ========== GETTING OWNER AND NETWORK ========== */
+
+	//let accounts = await ethers.getSigners();
+	//let owner = accounts[0];
 	let networkObj = await ethers.provider.getNetwork();
 	let network = networkObj.name;
+	const privateKey1 = process.env.PRIVATE_KEY;
+	const privateKey2 = process.env.PRIVATE_KEY_2;
+
 	if (network === 'unknown') {
 		network = 'localhost';
 	}
@@ -27,12 +33,19 @@ async function main() {
 		network = 'optimistic';
 	}
 
-	console.log('Account is: ' + owner.address);
+	const proxyOwner = new ethers.Wallet(privateKey1, ethers.provider);
+	const owner = new ethers.Wallet(privateKey2, ethers.provider);
+	
+	console.log('Owner is: ' + owner.address);
+	console.log('ProxyOwner is: ' + proxyOwner.address);
 	console.log('Network:' + network);
+	console.log('Network id:' + networkObj.chainId);
+
+    /* ========== PROPERTIES FOR INITIALIZE ========== */
 
 	const priceFeed = await ethers.getContractFactory('PriceFeed');
 	let priceFeedAddress = getTargetAddress('PriceFeed', network);
-	// TODO change reward token address
+	// TODO change reward token address to sUSD
 	let rewardTokenAddress = getTargetAddress('PriceFeed', network);
 
 	const min = 60;
@@ -53,50 +66,88 @@ async function main() {
 	const rounds = 6;
 	const buyIn = w3utils.toWei('10');
 
+    /* ========== DEPLOYMENT ========== */
+
 	// Dev env - deploy Thales.sol; Live env - use Thales.sol contract address
 	const ThalesRoyale = await ethers.getContractFactory('ThalesRoyale');
-	const ThalesRoyaleDeployed = await ThalesRoyale.deploy(
-		owner.address,
-		asset,
-		priceFeedAddress,
-		zeroAmount,
-		rewardTokenAddress,
-		rounds,
-		signUpPeriod,
-		roundChoosingLength,
-		roundLength,
-		claimTime,
-		season, 
-		buyIn,
-		false,
-		pauseBetweenSeasonsTime
-	);
-	await ThalesRoyaleDeployed.deployed();
-	// update deployments.json file
-	setTargetAddress('ThalesRoyale', network, ThalesRoyaleDeployed.address);
 
-	console.log('ThalesRoyale deployed to:', ThalesRoyaleDeployed.address);
+	const OwnedUpgradeabilityProxy = await ethers.getContractFactory('OwnedUpgradeabilityProxy');
+	const OwnedUpgradeabilityProxyDeployed = await OwnedUpgradeabilityProxy.connect(proxyOwner).deploy();
+
+	await OwnedUpgradeabilityProxyDeployed.deployed();
+	console.log('Owned proxy deployed on:', OwnedUpgradeabilityProxyDeployed.address);
+
+	const ThalesRoyaleConnected = await ThalesRoyale.connect(proxyOwner);
+	console.log("ThalesRoyale ready to deploy: ", ThalesRoyaleConnected.signer._isSigner);
+
+	const ThalesRoyaleDeployed = await ThalesRoyaleConnected.deploy();
+	await ThalesRoyaleDeployed.deployed();
+
+	console.log('Thales Royale logic contract deployed on:', ThalesRoyaleDeployed.address);
+	setTargetAddress('Thales Royale', network, ThalesRoyaleDeployed.address);
+
+	initializeRoyaleData = encodeCall(
+		'initialize',
+		['address', 'bytes32', 'address', 'uint', 'address', // 5
+		'uint','uint', 'uint', 'uint', 'uint', // 5
+		'uint', 'uint','bool', 'uint'], // 4
+		[
+			owner.address, 			//1
+			asset, 					//2
+			priceFeedAddress,		//3
+			zeroAmount,				//4
+			rewardTokenAddress,		//5
+			rounds,					//6
+			signUpPeriod,			//7
+			roundChoosingLength,	//8
+			roundLength,			//9
+			claimTime,				//10
+			season, 				//11
+			buyIn,					//12
+			false,					//13
+			pauseBetweenSeasonsTime	//14
+		]
+	);
+
+	let tx = await OwnedUpgradeabilityProxyDeployed.upgradeToAndCall(
+		ThalesRoyaleDeployed.address, initializeRoyaleData);
+	
+	await tx.wait().then(e => {
+		console.log('Proxy updated');
+	});
+
+	const ProxyThalesRoyaleDeployed = ThalesRoyale.connect(owner).attach(OwnedUpgradeabilityProxyDeployed.address);
+
+	tx = await ProxyThalesRoyaleDeployed.initialize(owner.address);
+
+	await tx.wait().then(e => {
+		console.log('ThalesRoyale deployed on:', ProxyThalesRoyaleDeployed.address);
+	});
+
+	setTargetAddress('ThalesRoyale', network, ProxyThalesRoyaleDeployed.address);
+
+	const aggregators = require(`./aggregators/${network}.json`);
+	for (let [key, aggregator] of Object.entries(aggregators)) {
+		let tx = await ProxyThalesRoyaleDeployed.addAggregator(toBytes32(key), aggregator);
+		await tx.wait().then(e => {
+			console.log('ThalesRoyale: addAggregator for', key);
+		});
+	}
+
+    /* ========== VEFIFICATION ========== */
 
 	await hre.run('verify:verify', {
 		address: ThalesRoyaleDeployed.address,
-		constructorArguments: [
-			owner.address,
-			asset,
-			priceFeedAddress,
-			zeroAmount,
-			rewardTokenAddress,
-			rounds,
-			signUpPeriod,
-			roundChoosingLength,
-			roundLength,
-			claimTime,
-			season, 
-			buyIn,
-			false,
-			pauseBetweenSeasonsTime
-		],
+		constructorArguments: initializeRoyaleData,
+	});
+
+	await hre.run('verify:verify', {
+		address: ProxyThalesRoyaleDeployed.address,
+		constructorArguments: [],
 	});
 }
+
+    /* ========== MAIN ========== */
 
 main()
 	.then(() => process.exit(0))

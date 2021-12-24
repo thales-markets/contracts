77eb3f5ecdb9499f4854f51851711a13db14544f
kirilantevski <kirilantevski@gmail.com>
2021-12-24 18:08:13 +0100

    - fix

--- contracts/interfaces/ISNXRewardsState.sol
+++ contracts/interfaces/ISNXRewardsState.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.5.16;
 
 
 
-interface ISNXRewards {
+interface ISNXRewardsState {
     /* ========== VIEWS / VARIABLES ========== */
-    function getAccountsDebtEntry(address account, uint index) public view returns (uint, uint);
+    function getAccountsDebtEntry(address account, uint index) external view returns (uint, uint);
     function setAccountsDebtEntry(address account, uint index, uint _debtPercentage, uint _debtEntryIndex) external;
 
 }
--- contracts/test-helpers/SNXRewardsState.sol
+++ contracts/test-helpers/SNXRewardsState.sol
@@ -1,15 +1,12 @@
 pragma solidity ^0.5.16;
 
-import "../interfaces/ISNXRewards.sol";
+import "../interfaces/ISNXRewardsState.sol";
 
 contract SNXRewardsState is ISNXRewardsState {
 
     uint public debtPercentage;
     uint public debtEntryIndex;
     
-    uint public totalRewards;
-    uint public totalFees;
-
     constructor() public {}
     /* ========== VIEWS / VARIABLES ========== */
     function getAccountsDebtEntry(address account, uint index) public view returns (uint debtPercentage, uint debtEntryIndex)  {

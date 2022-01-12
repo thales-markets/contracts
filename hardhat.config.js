c26e2d0cd883af6a8538f4eb5da384a9105192eb
dgornjakovic <dgornjakovic87@gmail.com>
2022-01-12 15:39:31 +0100

    fix SafeBox Deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -146,7 +146,7 @@ module.exports = {
 		},
 		optimisticKovan: {
 			gasPrice: 10000,
-			url: 'https://kovan.optimism.io',
+			url: 'https://optimism-kovan.infura.io/v3/' + INFURA,
 			accounts: [PRIVATE_KEY],
 		},
 	},

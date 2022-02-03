0010f08a7a3f1d974e5ff9f92424878b080adb13
kirilantevski <kirilantevski@gmail.com>
2022-02-03 16:55:20 +0100

    - success (sending and deployment) - works on a Linux machine - does not work for Windows WSL

--- scripts/l2_utility/usingHDwallet.js
+++ scripts/l2_utility/usingHDwallet.js
@@ -1,78 +1,39 @@
-// const { ethers } = require('hardhat');
 const hre = require('hardhat');
-const w3utils = require('web3-utils');
-const snx = require('synthetix-2.50.4-ovm');
-const { artifacts, contract, web3 } = require('hardhat');
-
 const sendToAddress = process.env.PERSONAL_ADD;
-
-const {LedgerSigner} = require('@anders-t/ethers-ledger');
-// const {LedgerSigner} = require("@ethersproject/hardware-wallets");
-
-const {
-	fastForward,
-	toUnit,
-	currentTime,
-	multiplyDecimalRound,
-	divideDecimalRound,
-} = require('../../test/utils/index')();
-
-const ZERO_ADDRESS = '0x' + '0'.repeat(40);
-
-const { getTargetAddress, setTargetAddress } = require('../helpers');
-
-const { toBytes32 } = require('../../index');
+const {LedgerSigner} = require("@ethersproject/hardware-wallets");
 
 async function main() {
-	// let accounts = await ethers.getSigners();
-	// let owner = accounts[0];
-	let networkObj = await hre.ethers.provider.getNetwork();
-    
+	
+	// IMPORTANT -------------------------------------------------->
+	// IN LINUX 
+	// Use command:  wget -q -O - https://raw.githubusercontent.com/LedgerHQ/udev-rules/master/add_udev_rules.sh | sudo bash
+	// to add the device 
+	
     const path = "m/44'/60'/0'/0";
-    // console.log(hre.ethers.provider);
-
-    const ledger = new LedgerSigner(hre.ethers.provider,path);
+    const ledger = new LedgerSigner(hre.ethers.provider,"hid",path);
     const ledger_network = await ledger.provider.getNetwork();
-	console.log("ledger net:",ledger_network);
-    // ledger.provider.connection = connection;
-	// console.log(networkObj);
-	let network = networkObj.name;
-	if (network == 'homestead') {
-		network = 'mainnet';
-	}
-	if (networkObj.chainId == 69) {
-		networkObj.name = 'optimisticKovan';
-		network = 'optimisticKovan';
-	}
-	if(networkObj.chainId == 10) {
-		networkObj.name = "optimistic";
-		network = 'optimistic'		
-	}
-
-	// console.log('Account is:' + owner.address);
-	// console.log('Network name:' + network);
+	
     let address = await ledger.getAddress();
+	let balance = await ledger.getBalance();
     console.log("Ledger path:", ledger.path);
-    console.log("\nLedger path:", address);
-
+    console.log("\nLedger address:", address);
+    console.log("\nLedger balance:", balance.toString());
+	
+    console.log("\nSign transaction");
     const tx = await ledger.sendTransaction({
-        to: sendToAddress,
-        value: ethers.utils.parseEther("0.0001")
+		to: sendToAddress,
+        value: ethers.utils.parseEther("0.002")
     });
-
-    // console.log("tx:\n", tx);
-    // const SnxRewards = await ethers.getContractFactory('TestThalesRoyale'); 
-    // const SNXRewards_connected = SnxRewards;
-    // // const SNXRewards_connected = await SnxRewards.connect(ledger);
-    // console.log(SNXRewards_connected);
-    // const SNXRewards_deployed = await SNXRewards_connected.deploy();
-    // await SNXRewards_deployed.deployed();
-
-    // console.log("Dummy contract deployed on:", SNXRewards_deployed.address);
     await tx.wait().then(e => {
-        console.log('Done transfer! $$$$ >');
-        });
-
+		console.log('Done transfer! $$$$ >');
+	});
+	
+    const SnxRewards = await ethers.getContractFactory('TestThalesRoyale'); 
+    const SNXRewards_connected = await SnxRewards.connect(ledger);
+	console.log("\nSign deployment");
+    const SNXRewards_deployed = await SNXRewards_connected.deploy();
+    await SNXRewards_deployed.deployed();
+	console.log('\nDummy contract deployed to:', SNXRewards_deployed.address);
 }
 
 main()

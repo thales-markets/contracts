156a5ec76db0889c06eb5537e32f55f6990b3881
kirilantevski <kirilantevski@gmail.com>
2022-02-03 12:56:22 +0100

    - no success

--- scripts/l2_utility/usingHDwallet.js
+++ scripts/l2_utility/usingHDwallet.js
@@ -4,9 +4,10 @@ const w3utils = require('web3-utils');
 const snx = require('synthetix-2.50.4-ovm');
 const { artifacts, contract, web3 } = require('hardhat');
 
-const INFURA = process.env.INFURA;
+const sendToAddress = process.env.PERSONAL_ADD;
 
 const {LedgerSigner} = require('@anders-t/ethers-ledger');
+// const {LedgerSigner} = require("@ethersproject/hardware-wallets");
 
 const {
 	fastForward,
@@ -29,11 +30,10 @@ async function main() {
     
     const path = "m/44'/60'/0'/0";
     // console.log(hre.ethers.provider);
-    let connection = {
-        url: 'https://optimism-kovan.infura.io/v3/'+INFURA
-    };
-    const ledger = new LedgerSigner(hre.ethers.provider, path);
 
+    const ledger = new LedgerSigner(hre.ethers.provider,path);
+    const ledger_network = await ledger.provider.getNetwork();
+	console.log("ledger net:",ledger_network);
     // ledger.provider.connection = connection;
 	// console.log(networkObj);
 	let network = networkObj.name;
@@ -51,16 +51,28 @@ async function main() {
 
 	// console.log('Account is:' + owner.address);
 	// console.log('Network name:' + network);
+    let address = await ledger.getAddress();
     console.log("Ledger path:", ledger.path);
+    console.log("\nLedger path:", address);
 
-    // console.log("Ledger accounts:\n", ledger);
-    const SnxRewards = await ethers.getContractFactory('SNXRewards'); 
-    const SNXRewards_connected = await SnxRewards.connect(ledger);
+    const tx = await ledger.sendTransaction({
+        to: sendToAddress,
+        value: ethers.utils.parseEther("0.0001")
+    });
+
+    // console.log("tx:\n", tx);
+    // const SnxRewards = await ethers.getContractFactory('TestThalesRoyale'); 
+    // const SNXRewards_connected = SnxRewards;
+    // // const SNXRewards_connected = await SnxRewards.connect(ledger);
     // console.log(SNXRewards_connected);
-    const SNXRewards_deployed = await SNXRewards_connected.deploy();
-    await SNXRewards_deployed.deployed();
+    // const SNXRewards_deployed = await SNXRewards_connected.deploy();
+    // await SNXRewards_deployed.deployed();
 
-    console.log("Dummy contract deployed on:", SNXRewards_deployed.address);
+    // console.log("Dummy contract deployed on:", SNXRewards_deployed.address);
+    await tx.wait().then(e => {
+        console.log('Done transfer! $$$$ >');
+        });
+
 }
 
 main()

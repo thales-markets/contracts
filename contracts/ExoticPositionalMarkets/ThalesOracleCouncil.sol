fb7bb8f68a1fe19ce5833dc0e24c3074f7d6b941
kirilantevski <kirilantevski@gmail.com>
2022-02-18 18:48:15 +0100

    - advancing

--- contracts/ExoticPositionalMarkets/ThalesOracleCouncil.sol
+++ contracts/ExoticPositionalMarkets/ThalesOracleCouncil.sol
@@ -11,7 +11,8 @@ import "../interfaces/IExoticPositionalMarket.sol";
 
 contract ThalesOracleCouncil is Initializable, ProxyOwned, PausableUpgradeable, ProxyReentrancyGuard  {
     using SafeMath for uint;
-
+    uint private constant COUNCIL_MAX_MEMBERS = 5;
+    uint private constant VOTING_OPTIONS = 8;
     mapping(uint => address) public councilMemberAddress;
     mapping(address => uint) public councilMemberIndex;
     uint public councilMemberCount;
@@ -24,12 +25,14 @@ contract ThalesOracleCouncil is Initializable, ProxyOwned, PausableUpgradeable, 
         uint disputeCode;
         uint disputeTimestamp;
     }
-
+    
     mapping(address => mapping(uint => Dispute)) public dispute;
     mapping(address => uint) public marketTotalDisputes;
     mapping(address => uint) public marketLastClosedDispute;
     mapping(address => bool) public marketClosedForDisputes;
 
+    mapping(address => mapping(uint => uint[])) public disputeVote;
+    mapping(address => mapping(uint => uint[VOTING_OPTIONS])) public disputeVotesCount;
 
     function initialize(
         address _owner,
@@ -70,6 +73,10 @@ contract ThalesOracleCouncil is Initializable, ProxyOwned, PausableUpgradeable, 
     function getDistputeCode(address _market, uint _index) external view returns(uint) {
         return dispute[_market][_index].disputeCode;
     }
+    
+    function getDistputeVotes(address _market, uint _index) external view returns(uint[] memory) {
+        return disputeVote[_market][_index];
+    }
 
     function isOracleCouncilMember(address _councilMember) public view returns(bool) {
         return (councilMemberIndex[_councilMember] > 0);
@@ -93,7 +100,7 @@ contract ThalesOracleCouncil is Initializable, ProxyOwned, PausableUpgradeable, 
         emit OracleCouncilMemberRemoved(_councilMember, councilMemberCount);
     }
 
-    function openDispute(address _market, string memory _disputeSting) external whenNotPaused {
+    function openDispute(address _market, string memory _disputeString) external whenNotPaused {
         require(IExoticPositionalMarket(_market).isMarketCreated(), "Market not created");
         require(!marketClosedForDisputes[_market], "Market is closed for disputes");
         require(
@@ -102,24 +109,45 @@ contract ThalesOracleCouncil is Initializable, ProxyOwned, PausableUpgradeable, 
                 );
         marketTotalDisputes[_market] = marketTotalDisputes[_market].add(marketTotalDisputes[_market]);
         dispute[_market][marketTotalDisputes[_market]].disputorAddress =  msg.sender;
-        dispute[_market][marketTotalDisputes[_market]].disputeString =  _disputeSting;
+        dispute[_market][marketTotalDisputes[_market]].disputeString =  _disputeString;
         dispute[_market][marketTotalDisputes[_market]].disputeTimestamp = block.timestamp;
+        disputeVote[_market][marketTotalDisputes[_market]] = new uint[](councilMemberCount);
+        emit NewDispute(_market, _disputeString, msg.sender);
     }
 
     function voteForDispute(address _market, uint _disputeIndex, uint _disputeCodeVote, uint _winningPosition) external onlyCouncilMembers {
         require(_disputeIndex > 0 && _disputeIndex > marketLastClosedDispute[_market], "Dispute non existent or already closed");
-        
+        require(_disputeCodeVote < VOTING_OPTIONS, "Invalid dispute code");
+        disputeVote[_market][_disputeIndex][councilMemberIndex[msg.sender]] = _disputeCodeVote;
+        disputeVotesCount[_market][_disputeIndex][_disputeCodeVote] = disputeVotesCount[_market][_disputeIndex][_disputeCodeVote].add(1);
+        if(decisionReachedOnDispute(_market, _disputeIndex) > 0) {
+            
+        }
+        emit VotedAddedForDispute(_market, _disputeIndex, _disputeCodeVote);
     }
     
     function closeDispute(address _market, uint _disputeIndex) external onlyCouncilMembers {
         require(_disputeIndex > 0 && _disputeIndex > marketLastClosedDispute[_market], "Dispute non existent or already closed");
 
+        emit MarketClosedForDisputes(_market, dispute[_market][marketTotalDisputes[_market]].disputeCode);
     }
 
+    function decisionReachedOnDispute(address _market, uint _disputeIndex) internal view returns(uint){
+        uint max = 0;
+        for(uint i=1; i< VOTING_OPTIONS; i++) {
+            max = (disputeVotesCount[_market][_disputeIndex][i] > max) ? disputeVotesCount[_market][_disputeIndex][i] : max;
+        }
+        return max;
+    }
+    
+    
     modifier onlyCouncilMembers() {
         require(isOracleCouncilMember(msg.sender), "Issuer not a council member");
         _;
     }
     event NewOracleCouncilMember(address councilMember, uint councilMemberCount);
     event OracleCouncilMemberRemoved(address councilMember, uint councilMemberCount);
+    event NewDispute(address market, string disputeString, address disputorAccount);
+    event VotedAddedForDispute(address market, uint disputeIndex, uint disputeCodeVote);
+    event MarketClosedForDisputes(address market, uint disputeFinalCode);
 }

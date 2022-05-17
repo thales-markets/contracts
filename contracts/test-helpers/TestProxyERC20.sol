5ecb2717a9b930559f269cffc77df4e4e0c9f12e
Ajla Musovic <ajla.msvc@gmail.com>
2022-05-17 14:49:05 +0200

    add test contracts

---
+++ contracts/test-helpers/TestProxyERC20.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.16;
+
+// Inheritance
+import "synthetix-2.50.4-ovm/contracts/ProxyERC20.sol";
+
+contract TestProxyERC20 is ProxyERC20 {}
---
+++ contracts/test-helpers/TestSynth.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.5.16;
+
+import "synthetix-2.50.4-ovm/contracts/Synth.sol";
+contract TestSynth is Synth {}

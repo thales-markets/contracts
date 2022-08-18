3df377e33a2001f04bd08212c7e7ea8b0fbf93fc
Gruja <gruja.work@gmail.com>
2022-08-18 10:55:28 +0200

    deployment on goerli
    deploy exotis USD to goerli
    
    deploy manager
    
    update goerli
    
    deploy SportPositionMastercopy
    
    update
    
    update

--- scripts/deploySportMarkets/deploySportPositionalMarkets/deploy_SportMarketFactory.js
+++ scripts/deploySportMarkets/deploySportPositionalMarkets/deploy_SportMarketFactory.js
@@ -34,6 +34,11 @@ async function main() {
 		network = 'optimisticEthereum';
 		PaymentToken = getTargetAddress('ProxysUSD', network);
 	}
+	if (networkObj.chainId == 5) {
+		networkObj.name = 'goerli';
+		network = 'goerli';
+		PaymentToken = getTargetAddress('ExoticUSD', network);
+	}
 
 	const SportMarketFactory = await ethers.getContractFactory('SportPositionalMarketFactory');
 	const SportMarketManager = await ethers.getContractFactory('SportPositionalMarketManager');

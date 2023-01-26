1d7ec7851fb5c69ef5590ec6f5893eb7915622ec
makiBaraba <markovic.ofsec@gmail.com>
2023-01-26 12:17:21 +0100

    deploy script for ToTNft

---
+++ scripts/deployTaleOfThales/deployNFT/deployTaleOfThalesNFTs.js
@@ -0,0 +1,72 @@
+const { ethers } = require('hardhat');
+const { getTargetAddress, setTargetAddress } = require('../../helpers');
+const w3utils = require('web3-utils');
+
+async function main() {
+	let accounts = await ethers.getSigners();
+	let owner = accounts[0];
+	let networkObj = await ethers.provider.getNetwork();
+	let network = networkObj.name;
+	let stakingAddress;
+
+	if (network === 'unknown') {
+		network = 'localhost';
+	}
+
+	if (network == 'homestead') {
+		network = 'mainnet';
+	}
+	if (networkObj.chainId == 69) {
+		networkObj.name = 'optimisticKovan';
+		network = 'optimisticKovan';
+	}
+	if (networkObj.chainId == 10) {
+		networkObj.name = 'optimisticEthereum';
+		network = 'optimisticEthereum';
+	}
+
+	if (networkObj.chainId == 80001) {
+		networkObj.name = 'polygonMumbai';
+		network = 'polygonMumbai';
+	}
+
+	if (networkObj.chainId == 137) {
+		networkObj.name = 'polygon';
+		network = 'polygon';
+	}
+	if (networkObj.chainId == 420) {
+		networkObj.name = 'optimisticGoerli';
+		network = 'optimisticGoerli';
+	}
+
+	if (networkObj.chainId == 5) {
+		networkObj.name = 'goerli';
+		network = 'goerli';
+	}
+
+	console.log('Account is: ' + owner.address);
+	console.log('Network:' + network);
+
+	stakingAddress = getTargetAddress('StakingThales', network);
+	console.log('Found stakingAddress at:' + stakingAddress);
+
+    const TaleOfThalesContract = await ethers.getContractFactory('TaleOfThalesNFTs');
+    const TaleOfThalesDeployed = await TaleOfThalesContract.deploy()
+
+	await TaleOfThalesDeployed.deployed();
+	setTargetAddress('TaleOfThalesNFTs', network, TaleOfThalesDeployed.address);
+
+	console.log('TaleOfThalesDeployed deployed to:', TaleOfThalesDeployed.address);
+
+	await hre.run('verify:verify', {
+		address: TaleOfThalesDeployed.address,
+		constructorArguments: [stakingAddress],
+	});
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});

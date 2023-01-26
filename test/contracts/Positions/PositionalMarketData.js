e4eefbfce2e79f88bfd82af6c014528ea41e7639
Vladan Poklopic <vladanpoklopic@gmail.com>
2023-01-26 12:01:03 +0100

    Add tests

---
+++ test/contracts/Positions/PositionalMarketData.js
@@ -0,0 +1,221 @@
+'use strict';
+
+const { artifacts, contract, web3 } = require('hardhat');
+const { toBN } = web3.utils;
+
+const { assert } = require('../../utils/common');
+const { fastForward, toUnit, currentTime } = require('../../utils')();
+const { toBytes32 } = require('../../../index');
+const { setupAllContracts } = require('../../utils/setup');
+
+const { convertToDecimals } = require('../../utils/helpers');
+
+let factory, manager, addressResolver;
+let PositionalMarket, priceFeed, oracle, sUSDSynth, PositionalMarketMastercopy, PositionMastercopy;
+let market, up, down, position, Synth;
+
+let aggregator_sAUD, aggregator_sETH, aggregator_sUSD, aggregator_nonRate;
+
+const ZERO_ADDRESS = '0x' + '0'.repeat(40);
+
+const MockAggregator = artifacts.require('MockAggregatorV2V3');
+
+contract('PostitionalMarketData', (accounts) => {
+	const [initialCreator, managerOwner, minter, dummy, exersicer, secondCreator, safeBox] = accounts;
+	const [creator, owner] = accounts;
+	let creatorSigner, ownerSigner;
+
+	const sUSDQty = toUnit(100000);
+	const sUSDQtyAmm = toUnit(1000);
+
+	const hour = 60 * 60;
+	const day = 24 * 60 * 60;
+
+	const sAUDKey = toBytes32('sAUD');
+	const sUSDKey = toBytes32('sUSD');
+	const sETHKey = toBytes32('sETH');
+	const nonRate = toBytes32('nonExistent');
+
+	const createMarket = async (man, oracleKey, strikePrice, maturity, initialMint, creator) => {
+		const tx = await man
+			.connect(creator)
+			.createMarket(oracleKey, strikePrice.toString(), maturity, initialMint.toString());
+		let receipt = await tx.wait();
+		const marketEvent = receipt.events.find(
+			(event) => event['event'] && event['event'] === 'MarketCreated'
+		);
+		return PositionalMarket.at(marketEvent.args.market);
+	};
+
+	before(async () => {
+		PositionalMarket = artifacts.require('PositionalMarket');
+	});
+
+	before(async () => {
+		Synth = artifacts.require('Synth');
+	});
+
+	before(async () => {
+		position = artifacts.require('Position');
+	});
+
+	before(async () => {
+		({
+			PositionalMarketManager: manager,
+			PositionalMarketFactory: factory,
+			PositionalMarketMastercopy: PositionalMarketMastercopy,
+			PositionMastercopy: PositionMastercopy,
+			AddressResolver: addressResolver,
+			PriceFeed: priceFeed,
+			SynthsUSD: sUSDSynth,
+		} = await setupAllContracts({
+			accounts,
+			synths: ['sUSD'],
+			contracts: [
+				'FeePool',
+				'PriceFeed',
+				'PositionalMarketMastercopy',
+				'PositionMastercopy',
+				'PositionalMarketFactory',
+			],
+		}));
+
+		[creatorSigner, ownerSigner] = await ethers.getSigners();
+
+		await manager.connect(creatorSigner).setPositionalMarketFactory(factory.address);
+
+		await factory.connect(ownerSigner).setPositionalMarketManager(manager.address);
+		await factory
+			.connect(ownerSigner)
+			.setPositionalMarketMastercopy(PositionalMarketMastercopy.address);
+		await factory.connect(ownerSigner).setPositionMastercopy(PositionMastercopy.address);
+
+		await manager.connect(creatorSigner).setTimeframeBuffer(0);
+		await manager.connect(creatorSigner).setPriceBuffer(toUnit(0.01).toString());
+
+		aggregator_sAUD = await MockAggregator.new({ from: managerOwner });
+		aggregator_sETH = await MockAggregator.new({ from: managerOwner });
+		aggregator_sUSD = await MockAggregator.new({ from: managerOwner });
+		aggregator_nonRate = await MockAggregator.new({ from: managerOwner });
+		aggregator_sAUD.setDecimals('8');
+		aggregator_sETH.setDecimals('8');
+		aggregator_sUSD.setDecimals('8');
+		const timestamp = await currentTime();
+
+		await aggregator_sAUD.setLatestAnswer(convertToDecimals(100, 8), timestamp);
+		await aggregator_sETH.setLatestAnswer(convertToDecimals(10000, 8), timestamp);
+		await aggregator_sUSD.setLatestAnswer(convertToDecimals(100, 8), timestamp);
+
+		await priceFeed.connect(ownerSigner).addAggregator(sAUDKey, aggregator_sAUD.address);
+
+		await priceFeed.connect(ownerSigner).addAggregator(sETHKey, aggregator_sETH.address);
+
+		await priceFeed.connect(ownerSigner).addAggregator(sUSDKey, aggregator_sUSD.address);
+
+		await priceFeed.connect(ownerSigner).addAggregator(nonRate, aggregator_nonRate.address);
+
+		await Promise.all([
+			sUSDSynth.issue(initialCreator, sUSDQty),
+			sUSDSynth.approve(manager.address, sUSDQty, { from: initialCreator }),
+			sUSDSynth.issue(minter, sUSDQty),
+			sUSDSynth.approve(manager.address, sUSDQty, { from: minter }),
+			sUSDSynth.issue(dummy, sUSDQty),
+			sUSDSynth.approve(manager.address, sUSDQty, { from: dummy }),
+		]);
+	});
+
+	let priceFeedAddress;
+	let rewardTokenAddress;
+	let ThalesAMM;
+	let thalesAMM, thalesAmmUtils;
+	let MockPriceFeedDeployed;
+	let MarketData;
+	let marketData;
+
+	beforeEach(async () => {
+		priceFeedAddress = owner;
+		rewardTokenAddress = owner;
+
+		let MockPriceFeed = artifacts.require('MockPriceFeed');
+		MockPriceFeedDeployed = await MockPriceFeed.new(owner);
+		await MockPriceFeedDeployed.setPricetoReturn(10000);
+
+		priceFeedAddress = MockPriceFeedDeployed.address;
+
+		const hour = 60 * 60;
+		ThalesAMM = artifacts.require('ThalesAMM');
+		thalesAMM = await ThalesAMM.new();
+		await thalesAMM.initialize(
+			owner,
+			priceFeedAddress,
+			sUSDSynth.address,
+			toUnit(1000),
+			owner,
+			toUnit(0.01),
+			toUnit(0.05),
+			hour * 2
+		);
+		await thalesAMM.setPositionalMarketManager(manager.address, { from: owner });
+		await thalesAMM.setImpliedVolatilityPerAsset(sETHKey, toUnit(120), { from: owner });
+		await thalesAMM.setSafeBoxData(safeBox, toUnit(0.01), { from: owner });
+		await thalesAMM.setMinMaxSupportedPriceAndCap(toUnit(0.05), toUnit(0.95), toUnit(1000), {
+			from: owner,
+		});
+
+		let ThalesAMMUtils = artifacts.require('ThalesAMMUtils');
+		thalesAmmUtils = await ThalesAMMUtils.new();
+		await thalesAMM.setAmmUtils(thalesAmmUtils.address, {
+			from: owner,
+		});
+
+		sUSDSynth.issue(thalesAMM.address, sUSDQtyAmm);
+
+		await factory.connect(ownerSigner).setThalesAMM(thalesAMM.address);
+
+		MarketData = artifacts.require('PositionalMarketData');
+		marketData = await MarketData.new();
+		await marketData.initialize(owner, { from: owner });
+
+		await marketData.setPositionalMarketManager(manager.address, { from: owner });
+		await marketData.setThalesAMM(thalesAMM.address, { from: owner });
+	});
+
+	const Position = {
+		UP: toBN(0),
+		DOWN: toBN(1),
+	};
+
+	describe('Test Market Data', () => {
+		it('test batch methods', async () => {
+			let now = await currentTime();
+			let newMarket = await createMarket(
+				manager,
+				sETHKey,
+				toUnit(12000),
+				now + day * 10,
+				toUnit(10),
+				creatorSigner
+			);
+
+			let priceUp = await thalesAMM.price(newMarket.address, Position.UP);
+
+			let options = await newMarket.options();
+			up = await position.at(options.up);
+			down = await position.at(options.down);
+			await up.approve(thalesAMM.address, toUnit(1205), { from: minter });
+
+			await newMarket.mint(toUnit(1205), {
+				from: minter,
+			});
+
+			let priceImpactForAllActiveMarkets = await marketData.getPriceImpactForAllActiveMarkets();
+			assert.bnEqual(priceImpactForAllActiveMarkets.length, 1);
+
+			let liquidityForAllActiveMarkets = await marketData.getLiquidityForAllActiveMarkets();
+			assert.bnEqual(liquidityForAllActiveMarkets.length, 1);
+
+			let pricesForAllActiveMarkets = await marketData.getPricesForAllActiveMarkets();
+			assert.bnEqual(pricesForAllActiveMarkets.length, 1);
+		});
+	});
+});

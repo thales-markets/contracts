95fe4f7123b0990a99f5442bcca3e3d7313bf679
Ajla Musovic <ajla.msvc@gmail.com>
2023-01-30 12:23:33 +0100

    fix tests

--- test/contracts/SportMarkets/ParlayAMM.js
+++ test/contracts/SportMarkets/ParlayAMM.js
@@ -1298,7 +1298,7 @@ contract('ParlayAMM', (accounts) => {
 			await voucher.setSportsAMM(SportsAMM.address, { from: owner });
 			await voucher.setParlayAMM(ParlayAMM.address, { from: owner });
 			await voucher.setPause(false, { from: owner });
-			await voucher.setTokenUris('', '', '', '', '', '', { from: owner });
+			await voucher.setTokenUris('', '', '', '', '', '', '', '', { from: owner });
 
 			Thales.approve(voucher.address, toUnit(20), { from: third });
 

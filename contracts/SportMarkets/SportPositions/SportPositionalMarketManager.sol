0294c878350896274d6395ab36276f0e7cf7eda7
KirilA <kirilantevski@gmail.com>
2023-02-16 14:50:40 +0100

    - new SportManager upgrade

--- contracts/SportMarkets/SportPositions/SportPositionalMarketManager.sol
+++ contracts/SportMarkets/SportPositions/SportPositionalMarketManager.sol
@@ -17,13 +17,12 @@ import "../../interfaces/ISportPositionalMarketManager.sol";
 import "../../interfaces/ISportPositionalMarket.sol";
 import "../../interfaces/ITherundownConsumer.sol";
 
-import "@openzeppelin/contracts-4.4.1/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-4.4.1/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "../../interfaces/IGamesOddsObtainer.sol";
 
 contract SportPositionalMarketManager is Initializable, ProxyOwned, ProxyPausable, ISportPositionalMarketManager {
     /* ========== LIBRARIES ========== */
-    using SafeERC20 for IERC20;
     using SafeMath for uint;
     using AddressSetLib for AddressSetLib.AddressSet;
 
@@ -378,7 +377,10 @@ contract SportPositionalMarketManager is Initializable, ProxyOwned, ProxyPausabl
         require(isKnownMarket(address(msg.sender)), "Market unknown.");
         amount = _transformCollateral(amount);
         amount = needsTransformingCollateral ? amount + 1 : amount;
-        sUSD.safeTransferFrom(sender, receiver, amount);
+        bool success = sUSD.transferFrom(sender, receiver, amount);
+        if (!success) {
+            revert("TransferFrom function failed");
+        }
     }
 
     function resolveMarket(address market, uint _outcome) external override {

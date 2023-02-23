0bb3dcc6505dc3b56dea70fd527999aeba50e2bf
makiBaraba <markovic.ofsec@gmail.com>
2023-02-23 08:29:55 +0100

    Changes for the contract and additional tests

--- contracts/SportMarkets/MarchMadness/MarchMadness.sol
+++ contracts/SportMarkets/MarchMadness/MarchMadness.sol
@@ -21,12 +21,14 @@ contract MarchMadness is ERC721URIStorage, Pausable, Ownable {
 
     uint public canNotMintOrUpdateAfter;
 
-    uint[61] public results;
+    uint[63] public results;
+    uint[6] public roundToPoints;
 
     mapping(address => bool) public addressAlreadyMinted;
 
-    mapping(uint => uint[61]) public itemToBrackets;
+    mapping(uint => uint[63]) public itemToBrackets;
     mapping(address => uint) public addressToTokenId;
+    mapping(uint => uint[]) public roundToGameIds;
 
     /* ========== CONSTRUCTOR ========== */
 
@@ -34,8 +36,8 @@ contract MarchMadness is ERC721URIStorage, Pausable, Ownable {
 
     /* ========== OWC ========== */
 
-    function mint(uint[61] memory _brackets) external whenNotPaused returns (uint newItemId) {
-        require(addressAlreadyMinted[msg.sender] == false, "Address already minted");
+    function mint(uint[63] memory _brackets) external whenNotPaused returns (uint newItemId) {
+        require(!addressAlreadyMinted[msg.sender], "Address already minted");
         require(block.timestamp < canNotMintOrUpdateAfter, "Can not mint after settled date");
 
         _tokenIds.increment();
@@ -52,20 +54,28 @@ contract MarchMadness is ERC721URIStorage, Pausable, Ownable {
         emit Mint(msg.sender, newItemId, _brackets);
     }
 
-    function updateBracketsForAlreadyMintedItem(uint _tokenId, uint[61] memory _brackets) external whenNotPaused {
+    function updateBracketsForAlreadyMintedItem(uint _tokenId, uint[63] memory _brackets) external whenNotPaused {
         require(_exists(_tokenId), "Item does not exists");
         require(ownerOf(_tokenId) == msg.sender, "Caller is not owner of entered tokenId");
+        require(block.timestamp < canNotMintOrUpdateAfter, "Can not update after settled date");
+
         itemToBrackets[_tokenId] = _brackets;
+
         emit UpdateBracketsForAlreadyMintedItem(msg.sender, _tokenId, _brackets);
     }
 
     /* ========== VIEW ========== */
 
-    function getCorrectPositionsByTokenId(uint _tokenId) public view returns (uint) {
-        uint[61] memory _brackets = itemToBrackets[_tokenId];
-        if (_brackets[1] == 0) return 0;
+    function isTeamWinnerOfGameId(uint _gameId, uint _teamId) public view returns (bool _flag) {
+        if (isValidGameId(_gameId)) {
+            if (results[_gameId] == _teamId) _flag = true;
+        }
+        return _flag;
+    }
 
-        uint correctPredictions = 0;
+    function getCorrectPositionsByTokenId(uint _tokenId) public view returns (uint correctPredictions) {
+        uint[63] memory _brackets = itemToBrackets[_tokenId];
+        if (_brackets[1] == 0) return 0;
 
         for (uint i = 0; i < _brackets.length; i++) {
             if (_brackets[i] == results[i]) {
@@ -81,6 +91,45 @@ contract MarchMadness is ERC721URIStorage, Pausable, Ownable {
         return getCorrectPositionsByTokenId(addressToTokenId[_minter]);
     }
 
+    function getCorrectPositionsPerRoundByTokenId(uint _roundId, uint _tokenId)
+        public
+        view
+        returns (uint correctPredictions)
+    {
+        if (!isValidRoundId(_roundId)) return 0;
+        if (!_exists(_tokenId)) return 0;
+
+        uint[] memory gameIdsForRound = roundToGameIds[_roundId];
+        uint[63] memory brackets = itemToBrackets[_tokenId];
+
+        for (uint i = 0; i < gameIdsForRound.length; i++) {
+            if (isTeamWinnerOfGameId(gameIdsForRound[i], brackets[gameIdsForRound[i]])) correctPredictions++;
+        }
+
+        return correctPredictions;
+    }
+
+    function getCorrectPositionsPerRoundByMinterAddress(uint _roundId, address _minter) public view returns (uint) {
+        if (addressToTokenId[_minter] == 0) return 0;
+        return getCorrectPositionsPerRoundByTokenId(_roundId, addressToTokenId[_minter]);
+    }
+
+    function getTotalPointsByTokenId(uint _tokenId) public view returns (uint totalPoints) {
+        if (!_exists(_tokenId)) return totalPoints;
+
+        for (uint i = 0; i < roundToPoints.length; i++) {
+            uint correctPositionPerRound = getCorrectPositionsPerRoundByTokenId(i, _tokenId);
+            totalPoints += (correctPositionPerRound * roundToPoints[i]);
+        }
+
+        return totalPoints;
+    }
+
+    function getTotalPointsByMinterAddress(address _minter) public view returns (uint) {
+        if (addressToTokenId[_minter] == 0) return 0;
+        return getTotalPointsByTokenId(addressToTokenId[_minter]);
+    }
+
     /* ========== INTERNALS ========== */
 
     function _beforeTokenTransfer(
@@ -91,32 +140,66 @@ contract MarchMadness is ERC721URIStorage, Pausable, Ownable {
         require(from == address(0) || to == address(0), "NonTransferrableERC721Token: non transferrable");
     }
 
+    function isValidGameId(uint _gameId) internal pure returns (bool _flag) {
+        if (_gameId >= 0 && _gameId < 63) _flag = true;
+        return _flag;
+    }
+
+    function isValidRoundId(uint _roundId) internal pure returns (bool _flag) {
+        if (_roundId >= 0 && _roundId < 6) _flag = true;
+        return _flag;
+    }
+
+    function isValidTeamIndex(uint _teamId) internal pure returns (bool _flag) {
+        if (_teamId > 0 && _teamId <= 64) _flag = true;
+        return _flag;
+    }
+
     /* ========== CONTRACT MANAGEMENT ========== */
 
-    function pause() public onlyOwner {
-        _pause();
+    function setPaused(bool paused) external onlyOwner {
+        return paused ? _pause() : _unpause();
     }
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
+    function setResultForGame(uint _gameId, uint _teamId) external onlyOwner {
+        require(isValidTeamIndex(_teamId), "Not valid team index");
+        require(isValidGameId(_gameId), "Not valid game index");
+        results[_gameId] = _teamId;
 
-    function setResultForGame(uint _game, uint _teamIndex) external onlyOwner {
-        require(_teamIndex < 69, "Not valid team index");
-        require(_game < 61, "Not valid game index");
-        results[_game] = _teamIndex;
-        emit ResultForGameAdded(_game, _teamIndex);
+        emit ResultForGameAdded(_gameId, _teamId);
     }
 
     function setFinalDateForPositioning(uint _toDate) external onlyOwner {
         canNotMintOrUpdateAfter = _toDate;
+
         emit FinalPositioningDateUpdated(_toDate);
+    }
+
+    function setPointsToRound(uint _roundId, uint _points) external onlyOwner {
+        require(isValidGameId(_roundId), "Not valid roundId");
+        roundToPoints[_roundId] = _points;
+
+        emit PointsSettledForRound(_roundId, _points);
+    }
+
+    function assignGameIdsToRound(uint _roundId, uint[] memory _gameIds) external onlyOwner {
+        // Validation of game ids
+        for (uint i = 0; i < _gameIds.length; i++) {
+            require(isValidGameId(_gameIds[i]), "Not valid gameId");
+        }
+        require(isValidRoundId(_roundId), "Not valid round id");
+        roundToGameIds[_roundId] = _gameIds;
+
+        emit GameIdsAssignedToRound(_roundId, _gameIds);
     }
 
     /* ========== EVENTS ========== */
 
-    event Mint(address _recipient, uint _id, uint[61] _brackets);
-    event UpdateBracketsForAlreadyMintedItem(address _minter, uint itemIndex, uint[61] _newBrackets);
-    event ResultForGameAdded(uint _gameIndex, uint _teamWinnerIndex);
+    event Mint(address _recipient, uint _id, uint[63] _brackets);
+    event UpdateBracketsForAlreadyMintedItem(address _minter, uint itemIndex, uint[63] _newBrackets);
+
+    event ResultForGameAdded(uint _gameIndex, uint _teamId);
     event FinalPositioningDateUpdated(uint _toDate);
+    event GameIdsAssignedToRound(uint _roundId, uint[] _gameIds);
+    event PointsSettledForRound(uint _roundId, uint _points);
 }
--- test/contracts/SportMarkets/MarchMadness.js
+++ test/contracts/SportMarkets/MarchMadness.js
@@ -12,7 +12,7 @@ contract('MarchMadness', (accounts) => {
 	let MarchMadnessContract;
 	let marchMadness;
 
-	const bracketsArray = Array(61).fill(1);
+	const bracketsArray = Array(63).fill(1);
 
 	beforeEach(async () => {
 		MarchMadnessContract = artifacts.require('MarchMadness');
@@ -29,7 +29,7 @@ contract('MarchMadness', (accounts) => {
 		});
 
 		it('Should revert, paused contract', async () => {
-			await marchMadness.pause({ from: owner });
+			await marchMadness.setPaused(true, { from: owner });
 
 			await expect(marchMadness.mint(bracketsArray, { from: first })).to.be.revertedWith(
 				'Pausable: paused'
@@ -42,9 +42,9 @@ contract('MarchMadness', (accounts) => {
 
 			const dateTo = new Date('01-12-2023').getTime();
 
-			await expect(marchMadness.setFinalDateForPositioning(dateTo, { from: first })).to.be.revertedWith(
-				'Ownable: caller is not the owner'
-			);
+			await expect(
+				marchMadness.setFinalDateForPositioning(dateTo, { from: first })
+			).to.be.revertedWith('Ownable: caller is not the owner');
 		});
 
 		it('Should set date range and emit event', async () => {
@@ -66,6 +66,24 @@ contract('MarchMadness', (accounts) => {
 			await expect(marchMadness.setResultForGame(1, 1, { from: first })).to.be.revertedWith(
 				'Ownable: caller is not the owner'
 			);
+		});
+
+		it('Should revert adding the points to round, then pass when it called by owner', async () => {
+			await expect(marchMadness.setPointsToRound(0, 2, { from: first })).to.be.revertedWith(
+				'Ownable: caller is not the owner'
+			);
+
+			await marchMadness.setPointsToRound(0, 2, { from: owner });
+
+			assert.bnEqual(await marchMadness.roundToPoints(0), 2);
+		});
+
+		it('Should assign gameIds to round', async () => {
+			const gameIdsArray = Array.from({ length: 32 }, (_, k) => k + 1);
+
+			await marchMadness.assignGameIdsToRound(0, gameIdsArray, { from: owner });
+
+			assert.bnEqual(await marchMadness.roundToGameIds(0, 4), 5);
 		});
 	});
 
@@ -108,7 +126,7 @@ contract('MarchMadness', (accounts) => {
 		});
 	});
 
-	describe('Updating minted positions/Getting corrent positions', () => {
+	describe('Updating minted positions/Getting correct positions', () => {
 		it('Should update already minted position, before that testing reverting on update brackets', async () => {
 			const dateTo = new Date('02-25-2023').getTime() / 1000;
 
@@ -118,8 +136,8 @@ contract('MarchMadness', (accounts) => {
 
 			assert.bnGt(await marchMadness.balanceOf(first), 0);
 
-			const newBrackets = Array.from({ length: 61 }, () =>
-				Math.floor(Math.random() == 0 ? 1 : Math.random() * 68)
+			const newBrackets = Array.from({ length: 63 }, () =>
+				Math.floor(Math.random() == 0 ? 1 : Math.random() * 64)
 			);
 
 			const newFirstPosition = 3;
@@ -147,7 +165,7 @@ contract('MarchMadness', (accounts) => {
 
 			await marchMadness.setFinalDateForPositioning(dateTo, { from: owner });
 
-			const newBrackets = Array.from({ length: 61 }, () => Math.floor((Math.random() + 0.1) * 68));
+			const newBrackets = Array.from({ length: 63 }, () => Math.floor((Math.random() + 0.1) * 64));
 
 			const newFirstPosition = 3;
 			const newSecondPosition = 4;
@@ -172,6 +190,163 @@ contract('MarchMadness', (accounts) => {
 
 			assert.bnEqual(await marchMadness.getCorrectPositionsByMinterAddress(first), 3);
 			assert.bnEqual(await marchMadness.getCorrectPositionsByMinterAddress(first), 3);
+		});
+
+		it('Should display count of correct positions from round, also show display points for that round', async () => {
+			// Setting the final date for positioning
+			const dateTo = new Date('02-25-2023').getTime() / 1000;
+			await marchMadness.setFinalDateForPositioning(dateTo, { from: owner });
+
+			// Setting gameIds for round
+			const gameIdsArray = Array.from({ length: 32 }, (_, k) => k + 1);
+			await marchMadness.assignGameIdsToRound(0, gameIdsArray, { from: owner });
+			assert.bnEqual(await marchMadness.roundToGameIds(0, 4), 5);
+
+			// Results
+			const resultsForForFirstRound = Array(32).fill(1);
+			resultsForForFirstRound[2] = 10;
+			resultsForForFirstRound[3] = 15;
+			resultsForForFirstRound[4] = 13;
+			resultsForForFirstRound[6] = 21;
+			resultsForForFirstRound[7] = 18;
+			resultsForForFirstRound[8] = 33;
+
+			for (let i = 0; i < resultsForForFirstRound.length; i++) {
+				await marchMadness.setResultForGame(i, resultsForForFirstRound[i], { from: owner });
+			}
+
+			const brackets = Array(63).fill(2);
+			brackets[2] = 10;
+			brackets[3] = 15;
+			brackets[4] = 13;
+			brackets[6] = 21;
+			brackets[7] = 18;
+			brackets[8] = 33;
+
+			await marchMadness.mint(brackets, { from: first });
+
+			assert.bnEqual(await marchMadness.getCorrectPositionsPerRoundByTokenId(0, 1), 6);
+
+			await marchMadness.setPointsToRound(0, 12, { from: owner });
+
+			assert.bnEqual(await marchMadness.getTotalPointsByTokenId(1), 6 * 12);
+		});
+	});
+
+	describe('Multiple rounds final testing', () => {
+		it('Should return count of correct positions and total points', async () => {
+			// Setting the final date for positioning
+			const dateTo = new Date('02-25-2023').getTime() / 1000;
+			await marchMadness.setFinalDateForPositioning(dateTo, { from: owner });
+
+			// Setting gameIds for round
+			const gameIdsForFirstRound = Array.from({ length: 32 }, (_, k) => k);
+			const gameIdsForSecondRound = Array.from({ length: 16 }, (_, k) => k + 31);
+			const gameIdsForThirdRound = Array.from({ length: 8 }, (_, k) => k + 47);
+			const gameIdsForFourthRound = Array.from({ length: 4 }, (_, k) => k + 55);
+			const gameIdsForFifthRound = [60, 61];
+			const gameIdsForSixthRound = [62];
+
+			await marchMadness.assignGameIdsToRound(0, gameIdsForFirstRound, { from: owner });
+			await marchMadness.assignGameIdsToRound(1, gameIdsForSecondRound, { from: owner });
+			await marchMadness.assignGameIdsToRound(2, gameIdsForThirdRound, { from: owner });
+			await marchMadness.assignGameIdsToRound(3, gameIdsForFourthRound, { from: owner });
+			await marchMadness.assignGameIdsToRound(4, gameIdsForFifthRound, { from: owner });
+			await marchMadness.assignGameIdsToRound(5, gameIdsForSixthRound, { from: owner });
+
+			// Set results for games
+			const results = Array(63).fill(1);
+
+			// First round
+			results[7] = 15;
+			results[10] = 23;
+			results[15] = 18;
+			results[17] = 41;
+			results[18] = 44;
+			results[25] = 12;
+
+			// Second round
+			results[34] = 15;
+			results[36] = 23;
+			results[38] = 18;
+			results[39] = 41;
+			results[41] = 12;
+
+			// Third round
+			results[49] = 15;
+			results[51] = 18;
+			results[52] = 41;
+			results[54] = 23;
+
+			// Fourth round
+			results[56] = 18;
+			results[57] = 23;
+			results[58] = 41;
+
+			// Fifth round
+			results[60] = 18;
+
+			// Final
+			results[62] = 18;
+
+			for (let i = 0; i < results.length; i++) {
+				await marchMadness.setResultForGame(i, results[i], { from: owner });
+			}
+
+			const brackets = Array(63).fill(3);
+
+			// First round
+			brackets[7] = 15;
+			brackets[10] = 23;
+			brackets[15] = 18;
+			brackets[17] = 41;
+			brackets[18] = 44;
+			brackets[25] = 12;
+
+			// Second round
+			brackets[34] = 15;
+			brackets[36] = 23;
+			brackets[38] = 18;
+			brackets[39] = 41;
+			brackets[41] = 12;
+
+			// Third round
+			brackets[49] = 15;
+			brackets[51] = 18;
+			brackets[52] = 41;
+			brackets[54] = 23;
+
+			// Fourth round
+			brackets[56] = 18;
+			brackets[57] = 23;
+			brackets[58] = 41;
+
+			// Fifth round
+			brackets[60] = 18;
+
+			// Final
+			brackets[62] = 18;
+
+			await marchMadness.mint(brackets, { from: first });
+
+			assert.bnEqual(await marchMadness.getCorrectPositionsPerRoundByTokenId(0, 1), 6);
+			assert.bnEqual(await marchMadness.getCorrectPositionsPerRoundByTokenId(1, 1), 5);
+			assert.bnEqual(await marchMadness.getCorrectPositionsPerRoundByTokenId(2, 1), 4);
+			assert.bnEqual(await marchMadness.getCorrectPositionsPerRoundByTokenId(3, 1), 3);
+			assert.bnEqual(await marchMadness.getCorrectPositionsPerRoundByTokenId(4, 1), 1);
+			assert.bnEqual(await marchMadness.getCorrectPositionsPerRoundByTokenId(5, 1), 1);
+
+			await marchMadness.setPointsToRound(0, 1, { from: owner });
+			await marchMadness.setPointsToRound(1, 2, { from: owner });
+			await marchMadness.setPointsToRound(2, 4, { from: owner });
+			await marchMadness.setPointsToRound(3, 7, { from: owner });
+			await marchMadness.setPointsToRound(4, 10, { from: owner });
+			await marchMadness.setPointsToRound(5, 20, { from: owner });
+
+			assert.bnEqual(
+				await marchMadness.getTotalPointsByMinterAddress(first),
+				6 * 1 + 5 * 2 + 4 * 4 + 3 * 7 + 1 * 10 + 20 * 1
+			);
 		});
 	});
 });

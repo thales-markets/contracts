00127324d3084decb978f01fbb299f54387c5270
Ajla Musovic <ajla.msvc@gmail.com>
2023-03-08 09:42:20 +0100

    upgrade parlay vault on optimism

---
+++ scripts/deployVaults/ParlayVault/upgrade.js
@@ -0,0 +1,73 @@
+const { ethers, upgrades } = require('hardhat');
+const { getTargetAddress, setTargetAddress } = require('../../helpers');
+
+async function main() {
+	let accounts = await ethers.getSigners();
+	let owner = accounts[0];
+	let networkObj = await ethers.provider.getNetwork();
+	let network = networkObj.name;
+
+	let proxySUSD;
+
+	if (network === 'unknown') {
+		network = 'localhost';
+	}
+
+	if (network == 'homestead') {
+		network = 'mainnet';
+	}
+
+	if (networkObj.chainId == 69) {
+		networkObj.name = 'optimisticKovan';
+		network = 'optimisticKovan';
+	}
+	if (networkObj.chainId == 10) {
+		networkObj.name = 'optimisticEthereum';
+		network = 'optimisticEthereum';
+		proxySUSD = getTargetAddress('ProxysUSD', network);
+	}
+
+	if (networkObj.chainId == 80001) {
+		networkObj.name = 'polygonMumbai';
+		network = 'polygonMumbai';
+	}
+
+	if (networkObj.chainId == 137) {
+		networkObj.name = 'polygon';
+		network = 'polygon';
+	}
+
+	if (networkObj.chainId == 420) {
+		networkObj.name = 'optimisticGoerli';
+		network = 'optimisticGoerli';
+		proxySUSD = getTargetAddress('ExoticUSD', network);
+	}
+
+	console.log('Account is: ' + owner.address);
+	console.log('Network:' + network);
+
+	const vaultAddress = getTargetAddress('ParlayVault', network);
+	console.log('Found ParlayVault at:', vaultAddress);
+
+	const Vault = await ethers.getContractFactory('ParlayVault');
+	const implementation = await upgrades.prepareUpgrade(vaultAddress, Vault);
+
+	if (networkObj.chainId == 420) {
+		await upgrades.upgradeProxy(vaultAddress, Vault);
+		console.log('Vault upgraded');
+	}
+
+	console.log('ParlayVaultImplementation: ', implementation);
+	setTargetAddress('ParlayVaultImplementation', network, implementation);
+
+	await hre.run('verify:verify', {
+		address: implementation,
+	});
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});

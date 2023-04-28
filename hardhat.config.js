cce23215564717b8af422c5ab292e72263671faa
KirilA <kirilantevski@gmail.com>
2023-04-28 17:11:38 +0200

    - compiling errors for OZ upgrade contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -137,6 +137,24 @@ module.exports = {
 		file: AST_FILENAME,
 		ignores: 'test-helpers',
 	},
+	zksolc: {
+		version: '1.3.8',
+		compilerSource: 'binary',
+		settings: {
+			//compilerPath: "zksolc",  // optional. Ignored for compilerSource "docker". Can be used if compiler is located in a specific folder
+			experimental: {
+				dockerImage: 'matterlabs/zksolc', // Deprecated! use, compilerSource: "binary"
+				tag: 'latest', // Deprecated: used for compilerSource: "docker"
+			},
+			libraries: {}, // optional. References to non-inlinable libraries
+			isSystem: false, // optional.  Enables Yul instructions available only for zkSync system contracts and libraries
+			forceEvmla: false, // optional. Falls back to EVM legacy assembly if there is a bug with Yul
+			optimizer: {
+				enabled: true, // optional. True by default
+				mode: '3', // optional. 3 by default, z to optimize bytecode size
+			},
+		},
+	},
 	defaultNetwork: 'hardhat',
 	networks: {
 		hardhat: {
@@ -145,6 +163,7 @@ module.exports = {
 			allowUnlimitedContractSize: true,
 			gasPrice: GAS_PRICE,
 			initialDate: new Date(inflationStartTimestampInSecs * 1000).toISOString(),
+			zksync: false,
 			// Note: forking settings are injected at runtime by hardhat/tasks/task-node.js
 		},
 		localhost: {
@@ -152,26 +171,36 @@ module.exports = {
 			blockGasLimit: 30e6,
 			url: 'http://localhost:8545',
 			loggingEnabled: true,
+			zksync: false,
 		},
 		ropsten: {
 			gasPrice: 'auto',
 			url: 'https://ropsten.infura.io/v3/' + INFURA,
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 		goerli: {
 			gasPrice: 'auto',
 			url: 'https://goerli.infura.io/v3/' + INFURA,
 			accounts: [PRIVATE_KEY],
+			zksync: false,
+		},
+		zkTestnet: {
+			url: 'https://testnet.era.zksync.dev', // The testnet RPC URL of zkSync Era network.
+			ethNetwork: 'goerli', // The Ethereum Web3 RPC URL, or the identifier of the network (e.g. `mainnet` or `goerli`)
+			zksync: true,
 		},
 		kovan: {
 			gasPrice: 'auto',
 			url: 'https://kovan.infura.io/v3/' + INFURA,
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 		mainnet: {
 			gasPrice: 'auto',
 			url: 'https://mainnet.infura.io/v3/' + INFURA,
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 		optimisticLocal: {
 			url: LOCAL_OPT_IP,
@@ -179,6 +208,7 @@ module.exports = {
 				mnemonic: 'test test test test test test test test test test test junk',
 			},
 			gasPrice: 10000,
+			zksync: false,
 		},
 		optimisticEthereum: {
 			url: 'https://optimism-mainnet.infura.io/v3/' + INFURA,
@@ -188,32 +218,38 @@ module.exports = {
 			gasPrice: 10000,
 			url: 'https://goerli.optimism.io',
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 		polygonMumbai: {
 			url: 'https://polygon-mumbai.infura.io/v3/' + INFURA,
 			accounts: [PRIVATE_KEY],
 			gasPrice: 80000000000,
+			zksync: false,
 		},
 		polygon: {
 			url: 'https://polygon-mainnet.infura.io/v3/' + INFURA,
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 		bsc: {
 			url: 'https://bsc-dataseed.binance.org/',
 			chainId: 56,
 			//gasPrice: 5000000000,
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 		arbitrumOne: {
 			url: 'https://arbitrum-mainnet.infura.io/v3/' + INFURA,
 			chainId: 42161,
 			//gasPrice: 5000000000,
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 		optimisticGoerli: {
 			gasPrice: 10000,
 			url: 'https://goerli.optimism.io',
 			accounts: [PRIVATE_KEY],
+			zksync: false,
 		},
 	},
 

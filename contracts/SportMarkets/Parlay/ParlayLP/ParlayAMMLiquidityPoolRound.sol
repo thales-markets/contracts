7ae99f95ec5eed369316b7c8bb9e00c05ae32de9
KirilA <kirilantevski@gmail.com>
2023-05-11 16:01:59 +0200

    - initialize contracts

---
+++ contracts/SportMarkets/Parlay/ParlayLP/ParlayAMMLiquidityPoolRound.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+import "../../../interfaces/ISportPositionalMarket.sol";
+
+import "./ParlayAMMLiquidityPool.sol";
+
+contract ParlayAMMLiquidityPoolRound {
+    /* ========== LIBRARIES ========== */
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    /* ========== STATE VARIABLES ========== */
+
+    SportAMMLiquidityPool public liquidityPool;
+    IERC20Upgradeable public sUSD;
+
+    uint public round;
+    uint public roundStartTime;
+    uint public roundEndTime;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    bool public initialized;
+
+    function initialize(
+        address _liquidityPool,
+        IERC20Upgradeable _sUSD,
+        uint _round,
+        uint _roundStartTime,
+        uint _roundEndTime
+    ) external {
+        require(!initialized, "Already initialized");
+        initialized = true;
+        liquidityPool = SportAMMLiquidityPool(_liquidityPool);
+        sUSD = _sUSD;
+        round = _round;
+        roundStartTime = _roundStartTime;
+        roundEndTime = _roundEndTime;
+        sUSD.approve(_liquidityPool, type(uint256).max);
+    }
+
+    function updateRoundTimes(uint _roundStartTime, uint _roundEndTime) external onlyLiquidityPool {
+        roundStartTime = _roundStartTime;
+        roundEndTime = _roundEndTime;
+        emit RoundTimesUpdated(_roundStartTime, _roundEndTime);
+    }
+
+    function exerciseMarketReadyToExercised(ISportPositionalMarket market) external onlyLiquidityPool {
+        if (market.resolved()) {
+            (uint homeBalance, uint awayBalance, uint drawBalance) = market.balancesOf(address(this));
+            if (homeBalance > 0 || awayBalance > 0 || drawBalance > 0) {
+                market.exerciseOptions();
+            }
+        }
+    }
+
+    function moveOptions(
+        IERC20Upgradeable option,
+        uint optionsAmount,
+        address destination
+    ) external onlyLiquidityPool {
+        option.safeTransfer(destination, optionsAmount);
+    }
+
+    modifier onlyLiquidityPool() {
+        require(msg.sender == address(liquidityPool), "only the Pool manager may perform these methods");
+        _;
+    }
+
+    event RoundTimesUpdated(uint _roundStartTime, uint _roundEndTime);
+}

aa5c1650e948d546ddef47dc868dfdecba918843
Gruja <gruja.work@gmail.com>
2023-09-04 14:13:04 +0200

    fix tests after merge
    adding from my branch

--- test/contracts/SportMarkets/SportsAMMRiskManager.js
+++ test/contracts/SportMarkets/SportsAMMRiskManager.js
@@ -384,6 +384,18 @@ contract('SportsAMMRiskManager', (accounts) => {
 			{ from: owner }
 		);
 
+		let GamesPlayerProps = artifacts.require('GamesPlayerProps');
+		let GamesPlayerPropsDeployed = await GamesPlayerProps.new({ from: owner });
+		await GamesPlayerPropsDeployed.initialize(
+			owner,
+			TherundownConsumerDeployed.address,
+			verifier.address,
+			SportPositionalMarketManager.address,
+			fourth, // dummy at beggining
+			[4, 16],
+			{ from: owner }
+		);
+
 		await Thales.transfer(TherundownConsumerDeployed.address, toUnit('1000'), { from: owner });
 		await TherundownConsumerDeployed.setSportContracts(
 			wrapper,
@@ -391,6 +403,7 @@ contract('SportsAMMRiskManager', (accounts) => {
 			SportPositionalMarketManager.address,
 			verifier.address,
 			GamesOddsObtainerDeployed.address,
+			GamesPlayerPropsDeployed.address,
 			{
 				from: owner,
 			}

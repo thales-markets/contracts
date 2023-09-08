f911d17f1aa1a287e51b0f05d5b89922a7603083
KirilA <kirilantevski@gmail.com>
2023-09-08 17:00:44 +0200

    - last change

--- contracts/SportMarkets/Parlay/ParlayVerifier.sol
+++ contracts/SportMarkets/Parlay/ParlayVerifier.sol
@@ -386,6 +386,8 @@ contract ParlayVerifier {
                     sgpFee2 = ONE + (25 * ONE_PERCENT);
                 } else if (odds2 <= (48 * ONE_PERCENT) && odds1 <= (11 * ONE_PERCENT)) {
                     sgpFee2 = (sgpFee * 95 * ONE_PERCENT) / ONE;
+                } else if (odds2 <= (55 * ONE_PERCENT) && odds1 >= (74 * ONE_PERCENT)) {
+                    sgpFee2 = ONE + (15 * ONE_PERCENT);
                 } else {
                     sgpFee2 = sgpFee;
                 }

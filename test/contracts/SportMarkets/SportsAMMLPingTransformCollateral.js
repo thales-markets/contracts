d588c2fd1221b532780dc5a8263cbd369e92b5b3
dgornjakovic <dgornjakovic87@gmail.com>
2023-09-12 15:58:50 +0200

    fix tests

--- test/contracts/SportMarkets/SportsAMMLPingTransformCollateral.js
+++ test/contracts/SportMarkets/SportsAMMLPingTransformCollateral.js
@@ -159,7 +159,8 @@ contract('SportsAMM', (accounts) => {
 		testDAI,
 		Referrals,
 		SportsAMM,
-		SportAMMLiquidityPool;
+		SportAMMLiquidityPool,
+		multiCollateralOnOffRamp;
 
 	const game1NBATime = 1646958600;
 	const gameFootballTime = 1649876400;
@@ -428,23 +429,42 @@ contract('SportsAMM', (accounts) => {
 		let ERC20token = artifacts.require('Thales');
 		testDAI = await ERC20token.new();
 
-		let CurveSUSD = artifacts.require('MockCurveSUSD');
-		curveSUSD = await CurveSUSD.new(
+		let MultiCollateralOnOffRamp = artifacts.require('MultiCollateralOnOffRamp');
+		multiCollateralOnOffRamp = await MultiCollateralOnOffRamp.new();
+		await multiCollateralOnOffRamp.initialize(owner, Thales.address);
+
+		let MockPriceFeed = artifacts.require('MockPriceFeed');
+		let MockPriceFeedDeployed = await MockPriceFeed.new(owner);
+		await multiCollateralOnOffRamp.setPriceFeed(MockPriceFeedDeployed.address, { from: owner });
+		await MockPriceFeedDeployed.setPricetoReturn(toUnit(1), { from: owner });
+
+		await multiCollateralOnOffRamp.setSupportedAMM(SportsAMM.address, true, { from: owner });
+
+		await multiCollateralOnOffRamp.setSupportedCollateral(testUSDC.address, true, { from: owner });
+
+		await SportsAMM.setMultiCollateralOnOffRamp(multiCollateralOnOffRamp.address, true, {
+			from: owner,
+		});
+
+		let CurveMock = artifacts.require('CurveMock');
+		let curveMock = await CurveMock.new(
 			Thales.address,
 			testUSDC.address,
-			testUSDT.address,
-			testDAI.address
+			testUSDC.address,
+			testUSDC.address
 		);
 
-		await SportsAMM.setCurveSUSD(
-			curveSUSD.address,
-			testDAI.address,
+		await multiCollateralOnOffRamp.setCurveSUSD(
+			curveMock.address,
 			testUSDC.address,
-			testUSDT.address,
+			testUSDC.address,
+			testUSDC.address,
 			true,
-			toUnit(0.02),
+			toUnit('0.01'),
 			{ from: owner }
 		);
+
+		await Thales.transfer(curveMock.address, toUnit('1000'), { from: owner });
 
 		let SportAMMLiquidityPoolContract = artifacts.require('SportAMMLiquidityPool');
 		SportAMMLiquidityPool = await SportAMMLiquidityPoolContract.new();
@@ -501,7 +521,6 @@ contract('SportsAMM', (accounts) => {
 		);
 
 		await testUSDC.mint(first, toUnit(100000));
-		await testUSDC.mint(curveSUSD.address, toUnit(100000));
 		await testUSDC.approve(SportsAMM.address, toUnit(100000), { from: first });
 	});
 

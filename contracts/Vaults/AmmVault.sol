181aacfe8c74186175f9084692d29e843e9e015f
makiBaraba <markovic.ofsec@gmail.com>
2023-10-03 11:08:47 +0200

    Remove console library

--- contracts/Vaults/AmmVault.sol
+++ contracts/Vaults/AmmVault.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";

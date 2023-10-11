b9e10232aeef5d7182b3ca907bdbedd9450c53a7
Sasa Lotina <lotinasa@gmail.com>
2023-10-11 11:52:30 +0200

    Deploy Speed Markets Data contract on Goerli

--- scripts/deploySpeedMarkets/deploySpeedMarketsAMMData.js
+++ scripts/deploySpeedMarkets/deploySpeedMarketsAMMData.js
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require('hardhat');
 const { getImplementationAddress } = require('@openzeppelin/upgrades-core');
 
-const { getTargetAddress, setTargetAddress } = require('../../helpers');
+const { getTargetAddress, setTargetAddress } = require('../helpers');
 
 async function main() {
 	let accounts = await ethers.getSigners();

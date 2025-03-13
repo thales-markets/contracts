f60579ee16a3dd0eef253b4cb080286ee5fe39cd
Sasa Lotina <lotinasa@gmail.com>
2025-03-13 11:42:30 +0100

    Add script to set March Madness results

---
+++ scripts/deploySportMarkets/deployMarchMadnessV2/setResults.js
@@ -0,0 +1,111 @@
+const { ethers } = require('hardhat');
+const { getTargetAddress, txLog } = require('../../helpers');
+const w3utils = require('web3-utils');
+
+async function main() {
+	let accounts = await ethers.getSigners();
+	let owner = accounts[0];
+	let networkObj = await ethers.provider.getNetwork();
+	let network = networkObj.name;
+
+	if (network === 'unknown') {
+		network = 'localhost';
+	}
+
+	if (network == 'homestead') {
+		network = 'mainnet';
+	}
+
+	if (networkObj.chainId == 69) {
+		networkObj.name = 'optimisticKovan';
+		network = 'optimisticKovan';
+	}
+	if (networkObj.chainId == 10) {
+		networkObj.name = 'optimisticEthereum';
+		network = 'optimisticEthereum';
+	}
+	if (networkObj.chainId == 80001) {
+		networkObj.name = 'polygonMumbai';
+		network = 'polygonMumbai';
+	}
+
+	if (networkObj.chainId == 137) {
+		networkObj.name = 'polygon';
+		network = 'polygon';
+	}
+
+	if (networkObj.chainId == 420) {
+		networkObj.name = 'optimisticGoerli';
+		network = 'optimisticGoerli';
+	}
+
+	if (networkObj.chainId == 5) {
+		networkObj.name = 'goerli';
+		network = 'goerli';
+	}
+
+	if (networkObj.chainId == 42161) {
+		networkObj.name = 'arbitrumOne';
+		network = 'arbitrumOne';
+	}
+
+	if (networkObj.chainId == 42161) {
+		networkObj.name = 'arbitrumOne';
+		network = 'arbitrumOne';
+	}
+
+	if (networkObj.chainId == 8453) {
+		networkObj.name = 'baseMainnet';
+		network = 'baseMainnet';
+	}
+
+	if (networkObj.chainId == 11155420) {
+		networkObj.name = 'optimisticSepolia';
+		network = 'optimisticSepolia';
+	}
+
+	console.log('Account is: ' + owner.address);
+	console.log('Network:' + network);
+	console.log('Network id:' + networkObj.chainId);
+
+	try {
+		const MarchMadness = await ethers.getContractFactory('MarchMadnessV2');
+		const marchMadnessAddress = getTargetAddress('MarchMadnessV2', network);
+		console.log('Found MarchMadness at:', marchMadnessAddress);
+
+		const marchMadness = MarchMadness.attach(marchMadnessAddress);
+
+		const resultArray = [
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0,
+		];
+
+		console.log('Setting result array');
+		console.log('ResultArray -> ', resultArray);
+		const tx = await marchMadness.setResultArray(resultArray, {
+			from: owner.address,
+		});
+		await tx.wait().then((e) => {
+			txLog(tx, 'Tx log');
+		});
+		await delay(1500);
+
+		console.log('Script has finished.');
+	} catch (e) {
+		console.log('e ', e);
+	}
+}
+
+function delay(time) {
+	return new Promise(function (resolve) {
+		setTimeout(resolve, time);
+	});
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
